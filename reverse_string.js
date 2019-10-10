c28f7cdfd12056cce26d54171012f788aa159929
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2019-10-10 01:00:31 -0400

    reverse string

--- reverse_string.js
+++ reverse_string.js
@@ -3,6 +3,7 @@ function reverse(str){
     //create new array 
     //loop through split string
     //insert each character into new array 
+    //join new array 
 
     let newArray = []
     let splitArray = str.split("")
@@ -12,12 +13,29 @@ function reverse(str){
     for(let i = 0; i < splitArray.length; i++){
         newArray.unshift(splitArray[i])
     }
-
+    console.log(newArray)
     return newArray.join("")
    
 }
-
 console.log(reverse('sadza no nyama'))
 
-var sadza = "sadza"
+function reverse2(){
+    //check input 
+    if(!str || str.length < 2 || typeof str !== 'string'){
+        return 'hmm that is not good'
+    }
 
+    const backwards = [];
+    const totalItems = str.length -1;
+    for (let i = totalItems; i = 0; i-- ) {
+        backwards.push(str[i]);
+    }
+
+    return backwards.join('')
+}
+
+function reverse3(){
+    return str.split('').reverse().join('')
+}
+
+const reverse3 = str => str.split('').reverse().join('')
