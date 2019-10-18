7c7b4b1f1ae0f5dcb78df77b2b567eb15fb39da4
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2019-10-17 23:54:49 -0400

    hashes

---
+++ HashTables/hashes.js
@@ -0,0 +1,14 @@
+let user = {
+    age: 54,
+    name: 'Kylie',
+    magic: true,
+    scream: function(){
+        return 'ahhhh!'
+    }
+}
+
+user.spell = 'abra cadabra'
+
+console.log(user.age)
+console.log(user.spell)
+console.log(user.scream())
